a9768ce8db960a2806d90aa5e338f0d40e7e5688
Nasser Sanou <nassersanou23@gmail.com>
2018-08-31 16:12:08 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -17,21 +17,21 @@ class Hash
 end # class
 
 # USING WHILE TO LOOP ARGUMENT 
-# $animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
-# class Hash 
-#   def keys_of(*argument)
-#   arr = []
+$animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
+class Hash 
+  def keys_of(*argument)
+  arr = []
    
-#   $animals.each do |key, value|
-#       i = 0
-#     while i < argument.length   
-#       if argument[i] == value 
-#         arr.push(key)
-#       end #if
-#         i +=1
-#     end # while
-#     end #do
-#     arr
-#   end #def
-# end 
-# $animals.keys_of("Australia", "Panama")
+  $animals.each do |key, value|
+      i = 0
+    while i < argument.length   
+      if argument[i] == value 
+        arr.push(key)
+      end #if
+        i +=1
+    end # while
+    end #do
+    arr
+  end #def
+end 
+$animals.keys_of("Australia", "Panama")
